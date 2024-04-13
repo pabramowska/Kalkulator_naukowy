38f15480a87143df1edc3a054b6fd68f77509cad
Patrycja <pabramowska1@gmail.com>
2024-04-13 19:09:54 +0200

    added buttons to the gui

--- src/ui/UserInterface.java
+++ src/ui/UserInterface.java
@@ -1,12 +1,31 @@
 package ui;
+import javax.swing.*;
 import javax.swing.JFrame;
 
 public class UserInterface extends JFrame {
     public UserInterface() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        setSize(400,100);
+        JPanel panel = new JPanel();
+        panel.add(new JButton("1"));
+        panel.add(new JButton("2"));
+        panel.add(new JButton("3"));
+        panel.add(new JButton("4"));
+        panel.add(new JButton("5"));
+        panel.add(new JButton("6"));
+        panel.add(new JButton("7"));
+        panel.add(new JButton("8"));
+        panel.add(new JButton("9"));
+        panel.add(new JButton("00"));
+        panel.add(new JButton("0"));
+        panel.add(new JButton("."));
+
+        getContentPane().add(panel);
+
+        pack();
+        setSize(400,800);
         setVisible(true);
+
     }
 }
 
